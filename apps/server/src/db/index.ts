17879c5301bd5408a00c589dab31ba3c98440111
JustinRoderick <justinroderick258@gmail.com>
2025-05-22 23:54:44 -0400

    create pool

--- apps/server/src/db/index.ts
+++ apps/server/src/db/index.ts
@@ -1,3 +1,5 @@
 import { drizzle } from "drizzle-orm/node-postgres";
+import type { NodePgDatabase } from "drizzle-orm/node-postgres";
 
 export const db = drizzle(process.env.DATABASE_URL || "");
+const pool = db.$client;
