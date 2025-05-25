44f3c626b415f0ec804b6c7ac1fe574b67d1476a
JustinRoderick <justinroderick258@gmail.com>
2025-05-24 23:12:44 -0400

    update auth

--- apps/server/src/lib/auth.ts
+++ apps/server/src/lib/auth.ts
@@ -1,4 +1,3 @@
-
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "../db";
@@ -7,14 +6,21 @@ import * as schema from "../db/schema/auth";
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
-    
-    
+
     schema: schema,
   }),
-  trustedOrigins: [
-    process.env.CORS_ORIGIN || "",
-  ],
+  trustedOrigins: [process.env.CORS_ORIGIN || ""],
   emailAndPassword: {
     enabled: true,
-  }});
-
+    autoSignIn: true,
+    minPasswordLength: 8,
+  },
+  plugins: [],
+  session: {
+    expiresIn: 30 * 24 * 60 * 60,
+    cookieCache: {
+      enabled: true,
+      maxAge: 5 * 60,
+    },
+  },
+});
