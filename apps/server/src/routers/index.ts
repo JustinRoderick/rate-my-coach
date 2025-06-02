5638ac76103de86739ef8733e313cac4ae7025ee
JustinRoderick <justinroderick258@gmail.com>
2025-06-01 23:52:06 -0400

    add private test

--- apps/server/src/routers/index.ts
+++ apps/server/src/routers/index.ts
@@ -4,5 +4,11 @@ export const appRouter = router({
   healthCheck: publicProcedure.query(() => {
     return "OK";
   }),
+  privateData: protectedProcedure.query(({ ctx }) => {
+    return {
+      message: "This is private",
+      user: ctx.session.user,
+    };
+  }),
 });
 export type AppRouter = typeof appRouter;
